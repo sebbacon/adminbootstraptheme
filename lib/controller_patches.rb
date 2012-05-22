6e3af640258047ea93c98eb3319f2b27fe50f0b7
Seb Bacon <seb.bacon@gmail.com>
2012-05-22 11:58:58 +0100

    Patch the AdminGeneralController so the summary stats can be shown on the stats page

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -5,11 +5,23 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 require 'dispatcher'
+
 Dispatcher.to_prepare do
-    # Example adding an instance variable to the frontpage controller
-    GeneralController.class_eval do
-        def mycontroller
-            @say_something = "Greetings friend"
+    AdminGeneralController.class_eval do
+        def stats
+            # Overview counts of things
+            @public_body_count = PublicBody.count
+            
+            @info_request_count = InfoRequest.count
+            @outgoing_message_count = OutgoingMessage.count
+            @incoming_message_count = IncomingMessage.count
+            
+            @user_count = User.count
+            @track_thing_count = TrackThing.count
+            
+            @comment_count = Comment.count
+            @request_by_state = InfoRequest.count(:group => 'described_state')
+            @tracks_by_type = TrackThing.count(:group => 'track_type')
         end
     end
 end
