0d29d14c90ec440f558db9ab18aa14637206dd95
Robin Houston <robin.houston@gmail.com>
2012-06-03 22:12:39 +0100

    Fix routing patch
    The routing patch code in the example theme doesn’t work. This does.

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,12 +1,8 @@
 # Here you can override or add to the pages in the core website
 
-
-require 'dispatcher'
-Dispatcher.to_prepare do
-    ActionController::Routing::Routes.draw do |map|
-        # Add a route for admin.js to the general controller
-        map.with_options :controller => 'general' do |general|
-            general.admin_js '/adminbootstraptheme/javascripts/admin.js', :action => 'admin_js'
-        end
+ActionController::Routing::Routes.draw do |map|
+    # Add a route for admin.js to the general controller
+    map.with_options :controller => 'general' do |general|
+        general.admin_js '/adminbootstraptheme/javascripts/admin.js', :action => 'admin_js'
     end
 end
